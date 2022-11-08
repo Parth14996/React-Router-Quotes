df3ad132c7a9bd2cff886b8c15e58c9194557eb8
Parth Date <dateparth6@gmail.com>
2022-11-08 15:01:00 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import React, {  Suspense } from "react";
 import { Navigate, Route, Routes, Link } from "react-router-dom";
 import { Layout } from "./components/layout/Layout";
-import { AllQuotes } from "./pages/AllQuotes";
-import { NewQuote } from "./pages/new-quote";
-import { NotFound } from "./pages/NotFound";
+// import { AllQuotes } from "./pages/AllQuotes";
+// This component is Lazily loaded - import { NewQuote } from "./pages/new-quote";
+// import { NotFound } from "./pages/NotFound";
 import { QuoteDetail } from "./pages/QuoteDetail";
 import Comments from "./components/comments/Comments";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const NewQuote = React.lazy(() => import('./pages/NewQuote').then((module) => ({default: module.NewQuote})))
+// const NewQuote = lazy(() => import('./pages/NewQuote'))- this does not work
+const NotFound = React.lazy(() => import("./pages/NotFound").then((module) => ({default: module.NotFound})))
+const AllQuotes = React.lazy(() => import('./pages/AllQuotes').then((module) => ({default: module.AllQuotes})))
 
 
 function App() {
   return (
     <Layout>
+      <Suspense fallback={<div className="centered"><LoadingSpinner /></div>}>
       <Routes>
         <Route path='/' element={<Navigate replace to='/quotes' />} />
         <Route path='/quotes' element={<AllQuotes />} />
@@ -29,6 +37,7 @@ function App() {
         <Route path='/new-quote' element={<NewQuote />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
+      </Suspense>
     </Layout>
   );
 }
